6ccd56454cee9b085f0ef2f1a04db06b6dfd64e6
Vitor Jordão <srvitorlol@gmail.com>
2019-07-21 19:33:48 -0300

    Minha primeira função complexa

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -2,6 +2,19 @@
   (:gen-class))
 
 (def total-de-vidas 6)
+
+(defn perdeu [] (print "Você perdeu"))
+
+(defn jogo [vidas] 
+    (if (= vidas 0) 
+        (perdeu)
+        (do 
+            (print vidas)
+            (jogo (dec vidas))
+        )
+        
+    )
+)
   
 (defn -main
   "I don't do a whole lot ... yet."
